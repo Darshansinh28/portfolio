8285d9450f8237faf8e662d6b5a0271f3adf3582
Darshansinh Devda <84326827+Darshansinh28@users.noreply.github.com>
2025-04-29 00:03:11 -0400

    chnaged file path

--- src/components/canvas/Robot.jsx
+++ src/components/canvas/Robot.jsx
@@ -8,7 +8,7 @@ import { useAnimations } from "@react-three/drei";
 
 const Robot = () => {
   const group = useRef();
-  const { scene, animations } = useGLTF("/robot_playground.glb");
+  const { scene, animations } = useGLTF("./public/robot_playground.glb");
   const { actions } = useAnimations(animations, group);
 
   useEffect(() => {
